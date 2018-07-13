0cdd330a30a398c8703a57f11f132573304f4f24
mathwww <manomanmatec@gmail.com>
2018-07-13 18:14:15 -0300

    Últimas modificações no pom.xml

--- src/main/java/br/jus/stj/siscovi/dao/TotalMensalDAO.java
+++ src/main/java/br/jus/stj/siscovi/dao/TotalMensalDAO.java
@@ -15,13 +15,42 @@ public class TotalMensalDAO {
         this.connection = connection;
     }
 
-    public ArrayList<TotalMensal> getValoresCalculados(){
-        String sql = "";
+    public ArrayList<TotalMensal> getValoresCalculados(int codContrato){
+        String sql = "SELECT  u.nome AS \"Gestor\"," +
+                " c.nome_empresa AS \"Empresa\"," +
+                " 'Contrato Nº: ' + c.numero_contrato AS \"Contrato\"," +
+                " f.nome AS \"Função\"," +
+                " tmr.data_referencia AS \"Data referência\"," +
+                " ROUND(SUM(tmr.ferias), 2) AS \"Férias retido\"," +
+                " ROUND(SUM(tmr.terco_constitucional), 2) AS \"Terço constitucional retido\"," +
+                " ROUND(SUM(tmr.decimo_terceiro), 2) AS \"Décimo terceiro retido\"," +
+                " ROUND(SUM(tmr.incidencia_submodulo_4_1), 2) AS \"Incidência retido\"," +
+                " ROUND(SUM(tmr.multa_fgts), 2) AS \"MULTA do FGTS retido\"," +
+                " ROUND(SUM(tmr.ferias) + SUM(tmr.terco_constitucional) + SUM(tmr.decimo_terceiro) + SUM(tmr.incidencia_submodulo_4_1) + SUM(tmr.multa_fgts), 2) AS \"Total retido\"" +
+                " FROM tb_funcao_contrato fc" +
+                " JOIN tb_contrato c ON c.cod = fc.cod_contrato" +
+                " JOIN tb_funcao f ON f.cod = fc.cod_funcao" +
+                " JOIN tb_funcao_terceirizado ft ON ft.cod_funcao_contrato = fc.cod" +
+                " JOIN tb_terceirizado_contrato tc ON tc.cod = ft.cod_terceirizado_contrato" +
+                " JOIN tb_total_mensal_a_reter tmr ON tmr.cod_terceirizado_contrato = tc.cod" +
+                " JOIN tb_historico_gestao_contrato hgc ON hgc.cod_contrato = c.cod" +
+                " JOIN tb_usuario u ON u.cod = hgc.cod_usuario" +
+                " WHERE c.cod = ?" +
+                " AND MONTH(tmr.data_referencia) = ?" +
+                " AND YEAR(tmr.data_referencia) = ?" +
+                " AND hgc.cod_usuario = ?" +
+                " AND tmr.cod_funcao_terceirizado = ft.cod" +
+                " GROUP BY u.nome, " +
+                " c.nome_empresa," +
+                " 'Contrato Nº: ' + c.numero_contrato," +
+                " f.nome," +
+                " tmr.data_referencia" +
+                " ORDER BY 1,2,3,4";
         try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.executeUpdate();
 
         }catch(SQLException e){
-            e.printStackTrace();
+            throw new NullPointerException("Erro ao tentar carregar os cálculos de Total Mensal a Reter para o contrato: " + codContrato);
         }
         return null;
     }
