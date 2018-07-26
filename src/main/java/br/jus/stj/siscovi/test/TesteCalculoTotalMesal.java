46f9985cf764d3673fbb378493995938fcc178cb
mathwww <manomanmatec@gmail.com>
2018-07-26 16:44:24 -0300

    Classe movida do pacote 'calculos' para 'test'

---
+++ src/main/java/br/jus/stj/siscovi/test/TesteCalculoTotalMesal.java
@@ -0,0 +1,20 @@
+package br.jus.stj.siscovi.test;
+
+import br.jus.stj.siscovi.controllers.TotalMensalController;
+import br.jus.stj.siscovi.dao.ConnectSQLServer;
+import br.jus.stj.siscovi.dao.ContratoDAO;
+import br.jus.stj.siscovi.dao.TotalMensalDAO;
+import br.jus.stj.siscovi.model.TotalMensal;
+
+public class TesteCalculoTotalMesal {
+    public static void main(String[] args){
+
+        //ConnectSQLServer connectSQLServer = new ConnectSQLServer();
+        //TotalMensalAReter totalMensalAReter = new TotalMensalAReter(connectSQLServer.dbConnect());
+        // totalMensalAReter.CalculaTotalMensal(1, 8, 2016);
+       //TotalMensalDAO totalMensalDAO  = new TotalMensalDAO(connectSQLServer.dbConnect());
+       //totalMensalDAO.recuperaAnosDeCalculosAnteriores(1);
+        //new TotalMensalController().getValoresCalculados(1,8);
+        //
+    }
+}
