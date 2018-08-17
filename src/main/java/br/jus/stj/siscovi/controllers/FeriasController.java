c09019509735c876847548573425cc9bfce6dacf
mathwww <manomanmatec@gmail.com>
2018-08-17 19:19:54 -0300

    Primeira lista de férias para o tipo de de restituição: Movimentação adicionada bem como o Controller e o DAO respectivo à férias. Correção feita na query de Ferias na função 'DataPeriodoAquisistivo', onde havia uma subtração entre datas não aceita pelo SQL Server, substituida pela chamada da função DATEDIFF

---
+++ src/main/java/br/jus/stj/siscovi/controllers/FeriasController.java
@@ -0,0 +1,30 @@
+package br.jus.stj.siscovi.controllers;
+
+import br.jus.stj.siscovi.dao.ConnectSQLServer;
+import br.jus.stj.siscovi.dao.FeriasDAO;
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/ferias")
+public class FeriasController {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getTerceirizadosFerias={codigoContrato}/{tipoRestituicao}")
+    public Response getTerceirizadosParaFerias(@PathParam("codigoContrato") int codigoContrato, @PathParam("tipoRestituicao") String tipoRestituicao) {
+        Gson gson = new Gson();
+        ConnectSQLServer connectSQLServer = new ConnectSQLServer();
+        FeriasDAO feriasDAO= new FeriasDAO(connectSQLServer.dbConnect());
+        String json = "";
+        if(tipoRestituicao.equals("MOVIMENTACAO")) {
+           json = gson.toJson(feriasDAO.getListaTerceirizadoParaCalculoDeFerias(codigoContrato));
+        }
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+}
--- src/main/java/br/jus/stj/siscovi/dao/FeriasDAO.java
+++ src/main/java/br/jus/stj/siscovi/dao/FeriasDAO.java
@@ -1,12 +1,11 @@
 package br.jus.stj.siscovi.dao;
 
+import br.jus.stj.siscovi.calculos.Ferias;
 import br.jus.stj.siscovi.model.TerceirizadoFerias;
 
 import java.sql.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 
-import static java.time.temporal.TemporalAdjusters.firstDayOfYear;
 
 public class FeriasDAO {
     private final Connection connection;
@@ -15,41 +14,41 @@ public class FeriasDAO {
         this.connection = connection;
     }
 
+    /**
+     *
+     * @param codigoContrato
+     * @return
+     */
     public ArrayList<TerceirizadoFerias> getListaTerceirizadoParaCalculoDeFerias(int codigoContrato) {
         ArrayList<TerceirizadoFerias> terceirizados = new ArrayList<>();
         String sql = "SELECT TC.COD, " +
-                "T.NOME, " +
-                "TC.DATA_DISPONIBILIZACAO, " +
-                "TC.DATA_DESLIGAMENTO " +
-                "FROM tb_terceirizado_contrato TC " +
-                "JOIN " +
-                "tb_terceirizado T ON T.COD=TC.COD_TERCEIRIZADO " +
-                "WHERE COD_CONTRATO=?";
+                " T.NOME, " +
+                " TC.DATA_DISPONIBILIZACAO, " +
+                " TC.DATA_DESLIGAMENTO" +
+                " FROM tb_terceirizado_contrato TC " +
+                " JOIN " +
+                " tb_terceirizado T ON T.COD=TC.COD_TERCEIRIZADO " +
+                " WHERE COD_CONTRATO=? AND T.ATIVO='S'";
+
         try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, codigoContrato);
+            Ferias ferias = new Ferias(connection);
             try(ResultSet resultSet = preparedStatement.executeQuery()){
-                while(resultSet.next()) {
-                    Date inicioPeriodoAquisitivo = null;
-                    Date fimPeriodoAquisitivo;
-                    Date primeiroDiaDoAno = Date.valueOf(LocalDate.now().with(firstDayOfYear())); //Primeiro dia do ano
-                    if(resultSet.getDate("DATA_DISPONIBILIZACAO").before(primeiroDiaDoAno) && resultSet.getDate("DATA_DESLIGAMENTO") == null) {
-                        inicioPeriodoAquisitivo = primeiroDiaDoAno;
-                    }else if(resultSet.getDate("DATA_DISPONIBILIZACAO").after(primeiroDiaDoAno) || resultSet.getDate("DATA_DISPONIBILIZACAO").equals(primeiroDiaDoAno)) {
-                        inicioPeriodoAquisitivo = primeiroDiaDoAno;
-                    }
 
-                    if(resultSet.getDate("DATA_DESLIGAMENTO") == null) {
-                        fimPeriodoAquisitivo = Date.valueOf(inicioPeriodoAquisitivo.toLocalDate().plusYears(1));
-                    }else {
-                        fimPeriodoAquisitivo = resultSet.getDate("DATA_DESLIGAMENTO");
-                    }
+                while(resultSet.next()) {
+
+                    Date inicioPeriodoAquisitivo = ferias.DataPeriodoAquisitivo(resultSet.getInt("COD"), 1);
+                    Date fimPeriodoAquisitivo = ferias.DataPeriodoAquisitivo(resultSet.getInt("COD"), 2);
+
                     TerceirizadoFerias terceirizadoFerias = new TerceirizadoFerias(resultSet.getInt("COD"), resultSet.getString("NOME"), inicioPeriodoAquisitivo, fimPeriodoAquisitivo);
                     terceirizados.add(terceirizadoFerias);
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new NullPointerException("Nenhum funcionário ativo encontrado para este contrato.");
         }
         return terceirizados;
     }
+
+
 }
