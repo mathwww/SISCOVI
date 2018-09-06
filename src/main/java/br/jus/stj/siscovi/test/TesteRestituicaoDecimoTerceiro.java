f5fa522d6770a652bbe06716315efa522e0d49bc
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-09-06 15:34:32 -0300

    Modificações na restituição de férias.
    Incluídas a classe de modelo para valor da restituição de férias e editado o teste da classe de restiuição.

--- src/main/java/br/jus/stj/siscovi/test/TesteRestituicaoDecimoTerceiro.java
+++ src/main/java/br/jus/stj/siscovi/test/TesteRestituicaoDecimoTerceiro.java
@@ -1,10 +1,10 @@
 package br.jus.stj.siscovi.calculos;
 
 import br.jus.stj.siscovi.dao.ConnectSQLServer;
+import br.jus.stj.siscovi.helpers.ConsultaTSQL;
+import br.jus.stj.siscovi.model.ValorRestituicaoDecimoTerceiroModel;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
@@ -14,10 +14,54 @@ public class TesteRestituicaoDecimoTerceiro {
     public static void main(String[] args){
 
         ConnectSQLServer connectSQLServer = new ConnectSQLServer();
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
 
         RestituicaoDecimoTerceiro restituicaoDecimoTerceiro = new RestituicaoDecimoTerceiro(connectSQLServer.dbConnect());
+        ConsultaTSQL consulta = new ConsultaTSQL(connectSQLServer.dbConnect());
+        DecimoTerceiro decimoTerceiro = new DecimoTerceiro(connectSQLServer.dbConnect());
 
-        restituicaoDecimoTerceiro.CalculaRestituicaoDecimoTerceiro(729, "RESGATE",0, Date.valueOf("2016-08-05"),Date.valueOf("2016-12-31"),0);
+        int vCodContrato = consulta.RetornaCodContratoAleatorio();
+        int vCodTerceirizadoContrato = consulta.RetornaCodTerceirizadoAleatorio(vCodContrato);
+        String vTipoRestituicao = String.valueOf("RESGATE");
+        float vValorMovimentado = 1500;
+        int vNumeroParcela = 0;
+        String sqlDelete = "DELETE FROM TB_SALDO_RESIDUAL_DEC_TER; DELETE FROM TB_RESTITUICAO_DECIMO_TERCEIRO;";
+        Date vDataInicioContagem = decimoTerceiro.RetornaDataInicioContagem(vCodTerceirizadoContrato);
+        String vDataFim = String.valueOf(vDataInicioContagem.toLocalDate().getYear()) + "-12-31";
+
+        System.out.print(vDataFim);
+        Date vDataFimContagem = Date.valueOf(vDataFim);
+
+        System.out.print("Dados do teste\nCOD_CONTRATO: " + vCodContrato + " COD_TERCEIRIZADO_CONTRATO: " +
+                vCodTerceirizadoContrato + "\n");
+        System.out.print("Tipo de restituição: " + vTipoRestituicao + "\n" + "Data de início da contagem: " + vDataInicioContagem
+                + "\n" + "Data fim da contagem: " + vDataFimContagem + "\n");
+
+        ValorRestituicaoDecimoTerceiroModel restituicao = restituicaoDecimoTerceiro.CalculaRestituicaoDecimoTerceiro(
+                vCodTerceirizadoContrato, vNumeroParcela, vDataInicioContagem, vDataFimContagem);
+
+        System.out.println(restituicao.getValorDecimoTerceiro());
+        System.out.println(restituicao.getValorIncidenciaDecimoTerceiro());
+
+        restituicaoDecimoTerceiro.RegistraRestituicaoDecimoTerceiro(vCodTerceirizadoContrato,
+                vTipoRestituicao,
+                vNumeroParcela,
+                vDataInicioContagem,
+                restituicao.getValorDecimoTerceiro(),
+                restituicao.getValorIncidenciaDecimoTerceiro(),
+                vValorMovimentado);
+
+        try {
+
+            preparedStatement = connectSQLServer.dbConnect().prepareStatement(sqlDelete);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException sqle) {
+
+            throw new NullPointerException("Não foi possível deletar os dados de teste inseridos no banco.");
+
+        }
 
     }
 
